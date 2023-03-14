61714e05cc596b7d06331401b95f926dd75e1fdf
Santiago <san_chan97@hotmail.com>
2023-03-14 19:11:57 -0300

    update server test

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hola");
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var app = express();
+var PORT = 3000;
+app.get("/hola", function (req, res) {
+    res.json({
+        message: "hola mundo desde el servidor"
+    });
+});
+app.listen(PORT, function () {
+    console.log("Servidor corriendo en puerto", PORT);
+});
